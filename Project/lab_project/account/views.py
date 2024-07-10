5e6df347267ab3655bec73a58da54aabb0d3dd0a
fahmid267 <fahmid267@gmail.com>
2024-07-10 23:50:00 +0600

    Login code

--- Project/lab_project/account/views.py
+++ Project/lab_project/account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
@@ -20,3 +21,22 @@ def signup(request):
         return redirect('/')
     else:
         return render(request, "signup.html")
+
+# def login(request):
+#     if request.method != "POST":
+#         return render(request, 'login.html')
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email = email, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, "invalid credentials")
+    else:
+        return render(request, "login.html")
+    
