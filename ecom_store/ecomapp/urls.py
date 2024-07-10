53215bc0ec0329c0aed61afcff0743ddf6a7a237
Abrarkhan88 <abrark.3957@gmail.com>
2024-07-10 18:38:25 +0600

    Edited some parts in connecting to database

---
+++ ecom_store/ecomapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('shop_list/', views.shop_list, name='shop_list'),
+]
---
+++ ecom_store/ecomapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Shop
+
+# Create your views here.
+def shop_list(request):
+    shops = Shop.objects.all()
+    return render(request, 'shop_list.html', {'shops': shops})
+
+def index(request):
+    return render(request, 'ecomapp/index.html')
+
+def login_view(request):
+    return render(request, 'ecomapp/login.html')
+
+def signup_view(request):
+    return render(request, 'ecomapp/signup.html')
