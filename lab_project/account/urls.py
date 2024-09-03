44bdfaa6dfa84524c3681134e7dee2b647a39c88
istiak133 <istiak.ahmed3@g.bracu.ac.bd>
2024-09-04 01:48:24 +0600

    forgot password with email verification

--- lab_project/account/urls.py
+++ lab_project/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+urlpatterns =[
     path('signup', views.signup, name = "signup"),
     path('login', views.user_login, name = "login"),
     path('logout', views.user_logout, name = "logout"),
@@ -9,5 +10,10 @@ urlpatterns = [
     path('link_send/', views.link_send, name='link_send'),
     path('verify/<verf_link>', views.verify, name="verify"),
     # path('verified', views.)
-    path('verf_error', views.verf_error, name = "verf_error")
+    path('verf_error', views.verf_error, name = "verf_error"),
+    path("reset_password/",auth_views.PasswordResetViews.as_views(†emplate_name=templates/passresetmail.html),name="Reset_password"),
+    path("reset_password_sent/",auth_views.PasswordResetDoneViews.as_views(template_name=account/templates/passresetsent.html),name="password_reset_done"),
+    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmViews.as_views(template_name=account/templates/passresetform.html),name="password_reset_confirm"),
+    path("reset_password_complete/",auth_views.PasswordResetCompleteViews.as_views(template_name=/account/templates/passresetdone.html),name="password_reset_complete"),
+
 ]
--- lab_project/lab_project/settings.py
+++ lab_project/lab_project/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fahmid267@gmail.com'
-EMAIL_HOST_PASSWORD = 'rzchgzklhiasxtij'
+EMAIL_HOST_USER = 'istiakahmed.prj@gmail.com'
+EMAIL_HOST_PASSWORD = '471Project'
 
 AUTH_USER_MODEL = 'account.CustomUser'
 AUTHENTICATION_BACKENDS = [
