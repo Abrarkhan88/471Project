7fa085be6bd993ade556e40a72d4111c4f1b71e0
fahmid267 <fahmid267@gmail.com>
2024-09-15 03:44:25 +0600

    view_cart backend and frontend dev

--- lab_project/shopping_cart/urls.py
+++ lab_project/shopping_cart/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # path('view_cart', views.view_cart, name = "view_cart")
     path('add-to-cart/<int:product_id>', views.add_to_cart, name = "add_to_cart"), 
-    path('my_cart', views.view_cart, name = "view_cart"),
+    path('view_cart', views.view_cart, name = "view_cart"),
 ]
